7ae14f0ac1647d250ec1da639812abe541801075
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-01-08 21:03:56 -0700

    Added a variable called letter

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -8,25 +8,27 @@ class Program
         string userInput = Console.ReadLine();
         int percentage = int.Parse(userInput);
 
+        string letter = "";
+
         if (percentage >= 90)
         {
-            Console.WriteLine("A");
+            letter = "A";
         }
         else if (percentage >= 80)
         {
-            Console.WriteLine("B");
+            letter = "B";
         }
         else if (percentage >= 70)
         {
-            Console.WriteLine("C");
+            letter = "C";
         }
         else if (percentage >= 60)
         {
-            Console.WriteLine("D");
+            letter = "D";
         }
         else 
         {
-            Console.WriteLine("F");
+            letter = "F";
         }
         if (percentage >= 70)
         {
