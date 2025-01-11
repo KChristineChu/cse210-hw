7a4a939fba38cfc20b0787e3b33c8208a8bddc48
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-01-10 18:34:44 -0700

    Initialized variable number

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+       List<int> numList = new List<int>();
+
+       int number = -1;
+       while (number != 0) 
+       {
+            Console.Write("Enter a list of numbers, type 0 when finished. ");
+            
+            number = int.Parse(Console.ReadLine()); 
+
+            if (number != 0)
+            {
+                numList.Add(number);
+            }    
+       }
+        
+        
+        
+        
+        
     }
 }
