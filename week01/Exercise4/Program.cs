1f3238064a81c480486a62a95a3efb961fd0ca08
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-01-10 19:02:20 -0700

    Moved console.write max outside of foreach loop

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 class Program
 {
@@ -19,10 +21,27 @@ class Program
                 numList.Add(number);
             }    
        }
+        int sum = 0;
+        foreach (int num in numList)
+        {
+            sum += num;
+        }
         
+        Console.WriteLine($"The total is: {sum}");
         
-        
-        
-        
+        int average = sum/ numList.Count;
+
+        Console.WriteLine($"The average is: {average}");
+
+        int max = 0;
+        foreach (int num in numList)
+        {
+            if (num > max)
+            max = num;
+
+        }
+        Console.Write($"The maximum number is: {max}");
+
+
     }
 }
