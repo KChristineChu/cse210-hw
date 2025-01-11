6cc19b5b03cb0e176184807388fa96b4609485ba
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-01-11 15:43:10 -0700

    Correct variables

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+
+        int userNumber = PromptUserNumber();
+
+        int squareNumber = SquareNumber(userNumber);
     }
+
+    static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }    
+
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    } 
+
+    static int SquareNumber(int userNumber)
+    {
+        int squareNum = userNumber * userNumber;
+        return squareNum;
+    }   
 }
