eeb6a03e3cf01911d74949782790899c8181fb5d
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-01-14 23:58:35 -0700

    Fixed Job.cs codes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Google";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2011;
+        job1._endYear = 2018;
+        }
+
+        
+        
+        
+        
+
+        
     }
 }
