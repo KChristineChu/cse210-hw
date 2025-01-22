bbe16c190c711d69700abd76aee4bf2a27945271
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-01-21 23:47:55 -0700

    created methods

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -5,7 +5,34 @@ public class Fraction
     private int _topNum;
 
     private int _bottomNum;
+
+    public Fraction()
+    {
+        _topNum = 1;
+        _bottomNum = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _topNum = top;
+        _bottomNum = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _topNum = top;
+        _bottomNum = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_topNum} / {_bottomNum}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        double value = _topNum / _bottomNum;
+        return value;
+    }
 }
-
-
-
