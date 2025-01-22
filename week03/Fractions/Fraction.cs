8deb0d15aebee4d01bdd2997623f5f1205620e92
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-01-21 23:13:50 -0700

    set up class Fraction

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Fraction
+{
+    private int _topNum;
+
+    private int _bottomNum;
+}
+
+
+
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction();
+
+        Fraction fraction2 = new Fraction(5);
+
+        Fraction fraction3 = new Fraction(6, 7);
     }
 }
