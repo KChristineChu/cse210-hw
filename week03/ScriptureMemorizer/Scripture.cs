48cb0e5d76cf454c846bcfd3dc2271b21a663bd8
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-01-23 23:17:12 -0700

    Set up 3 files

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,29 @@
+public class Scripture
+{
+    private Reference _reference;
+
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        string name = "";
+        string[] words = name.Split("");
+        foreach (string word in words)
+        Console.Write(word);
+    
+
+    }
+    public void HideRandomWords(int numberToHide)
+    {
+        
+
+    }
+    public string GetDisplayText()
+    {
+
+    }
+    public bool IsCompletelyHidden()
+    {
+
+    }
+}
---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,35 @@
+using System.Security.Cryptography.X509Certificates;
+
+public class Word
+{
+    private string _text;
+
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+       _text = text;
+       _isHidden = false; 
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+
+    }
+    public void Show()
+    {
+
+    }
+    public bool IsHidden()
+    {
+        return false;
+    }
+    public string GetDisplayText()
+    {
+        return _text;
+
+    }
+    
+
+}
