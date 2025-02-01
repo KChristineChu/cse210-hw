6adb7a9863a78d84c2649be9d5690b263ec4e065
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-01-31 23:10:13 -0700

    Worked on Order file

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -1,7 +1,37 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 public class Order
 {
+    private List<Product> _products = new List<Product>();
     private Customer _customer;
-    private 
+
+    public float TotalCost(int productPrice, int quantity)
+    {
+        _productPrice = productPrice;
+        _quantity = quantity;
+        
+        return productPrice * quantity;
+    }
+
+
+//  public float GetTotalPrice()
+//  {
+//      float sum = 0;
+//      foreach ()
+//      sum +=
+//      finalSum = sum + shippingCost;
+//      return finalSum;
+//}
+
+    public void DisplayPackingLabel()
+    {
+        Console.WriteLine($"Product name: {_product1}, product ID: {_productId}");
+    } 
+
+    public void DisplayShippingLabel()
+    {
+        Console.WriteLine($"Customer Name: {_name}");
+        Console.WriteLine($"Address: {Address}");
+    }   
 }
