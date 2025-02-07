314f2a45c7483f8a2e4830d9a45dcd10c3b85d1a
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-02-06 18:07:18 -0700

    Worked on WritingAssignment.cs

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,15 +1,22 @@
 using System;
 using homework;
-
-
-class Program
+namespace homework
 {
-    static void Main(string[] args)
-    {
-       Assignment assignment = new ("Jones", "History");
-       Console.WriteLine(assignment.GetSummary());
 
-       MathAssignment mathAssignment = new ("Robert Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
-       Console.WriteLine(mathAssignment.GetHomeworkList());
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Assignment assignment = new ("Jones", "History");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new ("Robert Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new ("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+        }
+
+
     }
-}
+}    
