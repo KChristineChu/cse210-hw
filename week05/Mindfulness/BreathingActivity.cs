524e3864d5972e7004cf407a5bde9ba744db0303
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-02-06 23:45:50 -0700

    Laid out breathing file and listing file

---
+++ week05/Mindfulness/BreathingActivity.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Diagnostics;
+
+public class BreathingActivity : Activity
+{
+    public BreathingActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    //Console.WriteLine("Welcome to the Breathing Activity.");
+    //Console.WriteLine();
+    //Console.WriteLine("This activity will help you relax by walking you through breathing in and out slowly. Clear you mind and focus on your breathing.");
+    //Console.WriteLine();
+    //Console.WriteLine("How long, in seconds, would you like for your session? ")
+    //int _duration = Console.ReadLine();
+
+
+}
--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        Activity activity = new Activity();
+
+        while (true)
+        {
+            Console.WriteLine("Menu Options: ");
+            Console.WriteLine("1. Start breathing activity");
+            Console.WriteLine("2. Start reflecting activity");
+            Console.WriteLine("3. Start listing activity");
+            Console.WriteLine("4. Quit");
+
+            Console.Write("Select a choice from the menu: ");
+            string userInput = Console.ReadLine();
+
+            if (userInput == "1")
+            {
+                
+            }
+            else if (userInput == "2")
+            {
+                
+            }
+            else if (userInput == "3")
+            {
+                
+            }
+            else if (userInput == "4")
+            {
+                Console.WriteLine("Goodbye!");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Please choose 1, 2, 3 or 4");
+            }
+        }
+        
     }
 }
