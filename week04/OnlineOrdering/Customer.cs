7257bd7d6a1e83be7dc8b8ca5e7d63d1cbf6b33c
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-02-13 22:55:22 -0700

    Re-do Customer.cs

--- week04/OnlineOrdering/Customer.cs
+++ week04/OnlineOrdering/Customer.cs
@@ -1,6 +1,20 @@
 using System;
+using System.Net.Sockets;
 
 public class Customer
 {
-    private string name;
+    private string _customer;
+    private Address _address;
+
+    public Customer(string customer, Address address)
+    {
+        _customer = customer;
+        _address = address;
+
+    }
+    public bool LiveInUSA()
+    {
+        return _address.IsUsa();
+    }
+
 }
